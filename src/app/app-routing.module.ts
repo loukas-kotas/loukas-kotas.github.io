fc7237225a57b0e65f3fcf02b596f965033d6daf
loukas.kotas <loukas.kotas@gmail.com>
2019-08-03 20:40:57 +0300

    config routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'main',
+    loadChildren: './main/main.module#MainModule'
+  },
+  {
+    path: '**',
+    redirectTo: 'main'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(
+      routes,
+      {
+        useHash: true
+      }
+    )
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   declarations: [MainComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class MainModule { }
