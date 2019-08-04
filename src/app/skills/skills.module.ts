9f62ad80f50056c03721f1b889aaca8c25a6bb01
loukas.kotas <loukas.kotas@gmail.com>
2019-08-04 21:29:00 +0300

    import donu-chart module

--- src/app/skills/skills.module.ts
+++ src/app/skills/skills.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SkillsComponent } from './skills.component';
+import { DonutChartModule } from 'src/shared/components/donut-chart/donut-chart.module';
 
 
 
 @NgModule({
   declarations: [SkillsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    DonutChartModule
   ],
   exports: [
     SkillsComponent
