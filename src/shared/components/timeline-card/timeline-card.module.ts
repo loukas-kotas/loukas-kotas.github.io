23e6ea07e7914d5d49af1f844e7d931887e638f0
loukas.kotas <loukas.kotas@gmail.com>
2019-08-05 01:28:38 +0300

    init timeline-card module, component

---
+++ src/shared/components/timeline-card/timeline-card.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TimelineCardComponent } from './timeline-card.component';
+
+
+
+@NgModule({
+  declarations: [TimelineCardComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class TimelineCardModule { }
