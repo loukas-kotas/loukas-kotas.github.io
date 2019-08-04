354c1ab080fd8429013cdbf1906a7d599c7fd6f4
loukas.kotas <loukas.kotas@gmail.com>
2019-08-04 18:07:50 +0300

    init person-profile module, component

---
+++ src/app/person-profile/person-profile.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PersonProfileComponent } from './person-profile.component';
+
+
+
+@NgModule({
+  declarations: [PersonProfileComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PersonProfileModule { }
