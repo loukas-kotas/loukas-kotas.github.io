a81beed4a5488a4d2cbc586aae1b1ed75203dcad
loukas.kotas <loukas.kotas@gmail.com>
2019-08-04 18:30:59 +0300

    init donut-chart module, component

---
+++ src/shared/components/donut-chart/donut-chart.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DonutChartComponent } from './donut-chart.component';
+
+
+
+@NgModule({
+  declarations: [DonutChartComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class DonutChartModule { }
