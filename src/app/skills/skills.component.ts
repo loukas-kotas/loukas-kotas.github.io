c0d40223429b0ee43d2bf0b36b8839f321d52ce2
loukas.kotas <loukas.kotas@gmail.com>
2019-08-09 00:07:04 +0300

    skills: display skills when they are in viewport

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, AfterViewInit, AfterContentChecked, AfterViewChecked, OnDestroy } from '@angular/core';
+import { EventsService } from 'src/shared/services/events.service';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.scss']
 })
-export class SkillsComponent implements OnInit {
+export class SkillsComponent implements OnInit, OnDestroy, OnChanges, AfterViewChecked {
 
-  constructor() { }
+  el: HTMLElement;
+  showSkills = false;
+
+  constructor(
+    private eventsService: EventsService
+  ) { }
 
   ngOnInit() {
+    window.addEventListener('scroll', this.scroll, true); //third parameter
   }
+
+  ngOnDestroy() {
+    window.removeEventListener('scroll', this.scroll, true);
+  }
+
+  scroll = (): void => {
+    this.el = document.getElementById('skills-donuts');
+    const isInViewport = this.eventsService.isInViewport(this.el);
+
+    if ( isInViewport ) {
+      this.showSkills = true;
+    }
+
+  };
+
 
 }
