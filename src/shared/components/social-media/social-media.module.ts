0b9d19e9087bf13ac68802dccf7e7db36f63d608
loukas.kotas <loukas.kotas@gmail.com>
2019-08-09 00:19:01 +0300

    init social-media module, component

---
+++ src/shared/components/social-media/social-media.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SocialMediaComponent } from './social-media.component';
+
+
+
+@NgModule({
+  declarations: [SocialMediaComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    SocialMediaModule
+  ]
+})
+export class SocialMediaModule { }
