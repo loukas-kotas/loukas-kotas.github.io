61a74d9582ab6c0ca18206a74679f1fc9c788a53
loukas.kotas <loukas.kotas@gmail.com>
2019-08-09 00:04:40 +0300

    events.service: config logic, detect if element is in viewport

--- src/shared/services/events.service.ts
+++ src/shared/services/events.service.ts
@@ -6,4 +6,23 @@ import { Injectable } from '@angular/core';
 export class EventsService {
 
   constructor() { }
+
+  /**
+   *
+   * @param element
+   * @description
+   * Detect if an element is in the Viewport.
+   * @source https://gist.github.com/jjmu15/8646226
+   */
+  isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    const html = document.documentElement;
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= ( (window.innerHeight || html.clientHeight) + element.clientHeight ) &&
+      rect.right <= (window.innerWidth || html.clientWidth)
+    );
+  }
+
 }
