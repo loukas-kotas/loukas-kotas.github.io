7231d6944669419b26c3908bd1759f36586c6e6b
loukas.kotas <loukas.kotas@gmail.com>
2019-08-11 00:04:32 +0300

    scroll-down: config logic, layout, style

--- src/shared/components/scroll-down-button/scroll-down-button.component.ts
+++ src/shared/components/scroll-down-button/scroll-down-button.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-scroll-down-button',
   templateUrl: './scroll-down-button.component.html',
   styleUrls: ['./scroll-down-button.component.scss']
 })
-export class ScrollDownButtonComponent implements OnInit {
+export class ScrollDownButtonComponent implements OnInit, OnChanges {
+
+  @Input() show: boolean;
+  lastKnownScrollPosition: number;
+  yScrollThreshold = 50;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges() {
+    this.onScroll();
+  }
+
+  onScroll(): void {
+    window.addEventListener('scroll', (event) => {
+      this.hideScrollButton();
+    });
+  }
+
+  hideScrollButton(): void {
+    this.lastKnownScrollPosition = window.scrollY;
+    if ( this.lastKnownScrollPosition > this.yScrollThreshold ) {
+      this.show = false;
+    } else {
+      this.show = true;
+    }
   }
 
 }
