1d814b40f424893a881d4f4e3a9c837200af8601
loukas.kotas <loukas.kotas@gmail.com>
2019-08-11 00:05:07 +0300

    import scroll-down-button module

--- src/app/person-profile/person-profile.module.ts
+++ src/app/person-profile/person-profile.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PersonProfileComponent } from './person-profile.component';
+import { ScrollDownButtonModule } from 'src/shared/components/scroll-down-button/scroll-down-button.module';
 
 
 
 @NgModule({
   declarations: [PersonProfileComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ScrollDownButtonModule
   ],
   exports: [
     PersonProfileComponent
