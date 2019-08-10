3875a0f26ebec606b118003c5fe80bc2c8088f04
loukas.kotas <loukas.kotas@gmail.com>
2019-08-11 02:28:55 +0300

    rotate star when scroll to top

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  scrollToTopCounter: number;
+  lastKnownScrollPosition: number;
+
   constructor() { }
 
   ngOnInit() {
+    this.scrollToTopCounter = 0;
+    this.onScroll();
+    setTimeout(() => {
+      this.rotateIcon();
+    }, 1000);
   }
+
+  rotateIcon(): void {
+    if ( this.scrollToTopCounter > 0 && this.lastKnownScrollPosition === 0) {
+      const iconEl = document.getElementById('brand-icon') as HTMLElement;
+      iconEl.classList.remove('navbar-brand--animation');
+      setTimeout(() => {
+        iconEl.classList.add('navbar-brand--animation');
+      }, 20);
+    }
+  }
+
+  onScroll(): void {
+    window.addEventListener('scroll', (event) => {
+      this.lastKnownScrollPosition = window.scrollY;
+      if (this.lastKnownScrollPosition === 0) {
+        this.scrollToTopCounter++;
+        console.log(this.scrollToTopCounter);
+      }
+      this.rotateIcon();
+    });
+  }
+
 
 }
