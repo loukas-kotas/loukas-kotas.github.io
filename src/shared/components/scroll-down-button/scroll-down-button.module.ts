25dc001e77d71940678f7ed1cd6792b648d66014
loukas.kotas <loukas.kotas@gmail.com>
2019-08-10 22:46:37 +0300

    init scroll-down module, component

---
+++ src/shared/components/scroll-down-button/scroll-down-button.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ScrollDownButtonComponent } from './scroll-down-button.component';
+
+
+
+@NgModule({
+  declarations: [ScrollDownButtonComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [ScrollDownButtonComponent]
+})
+export class ScrollDownButtonModule { }
