30a86254cf352af0ca8968aa0b3ea0445db686a4
loukas.kotas <loukas.kotas@gmail.com>
2019-08-11 19:23:59 +0300

    mock donut-component with an extended one

--- src/shared/components/donut-chart/donut-chart.component.spec.ts
+++ src/shared/components/donut-chart/donut-chart.component.spec.ts
@@ -4,32 +4,39 @@ import { DonutChartComponent } from './donut-chart.component';
 import { Component, Input } from '@angular/core';
 
 describe('DonutChartComponent', () => {
-  let component: TestDonutComponent;
-  let fixture: ComponentFixture<TestDonutComponent>;
+  let component: TestDonutChartComponent;
+  let fixture: ComponentFixture<TestDonutChartComponent>;
 
   @Component({
     selector: 'app-test-donut-chart',
     template: `<app-donut-chart [id]="1"></app-donut-chart>`
-  }) class TestDonutComponent {
-    @Input() id;
+  }) class TestDonutChartComponent extends DonutChartComponent {
+    constructor() {
+      super();
+      this.id = '1';
+      this.title = 'fake title';
+      this.circleColor = 'black';
+      this.circleBgdColor = 'white';
+    }
   }
+
+
 
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DonutChartComponent, TestDonutComponent ]
+      declarations: [ DonutChartComponent, TestDonutChartComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TestDonutComponent);
+    fixture = TestBed.createComponent(TestDonutChartComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    // component.ngAfterViewInit();
     expect(component).toBeTruthy();
   });
 });
