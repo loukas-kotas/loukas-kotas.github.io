e997d283d843ae56597599c99ca087d6ebf2ecf2
loukas.kotas <loukas.kotas@gmail.com>
2019-08-15 00:56:32 +0300

    import skill budges module

--- src/app/skills/skills.module.ts
+++ src/app/skills/skills.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SkillsComponent } from './skills.component';
 import { DonutChartModule } from 'src/shared/components/donut-chart/donut-chart.module';
+import { SkillBudgesModule } from 'src/shared/components/skill-budges/skill-budges.module';
 
 
 
@@ -9,7 +10,8 @@ import { DonutChartModule } from 'src/shared/components/donut-chart/donut-chart.
   declarations: [SkillsComponent],
   imports: [
     CommonModule,
-    DonutChartModule
+    DonutChartModule,
+    SkillBudgesModule
   ],
   exports: [
     SkillsComponent
