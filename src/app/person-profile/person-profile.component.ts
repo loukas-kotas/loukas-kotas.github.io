20b67df57604c05af17793588dae81c4b14ca494
loukas.kotas <loukas.kotas@gmail.com>
2019-08-14 20:48:53 +0300

    replace text

--- src/app/person-profile/person-profile.component.ts
+++ src/app/person-profile/person-profile.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class PersonProfileComponent implements OnInit {
 
   showScrollDownButton = false;
-  dataText = ['Loukas Kotas', 'Web Developer', 'Javascript Freak'];
+  dataText = ['Loukas Kotas', 'Web Developer', 'Javascript Engineer'];
 
   constructor() { }
 
