cfa92889b2ad33d09606641da67fa460a5bfadd8
loukas.kotas <loukas.kotas@gmail.com>
2019-08-18 18:54:19 +0300

    import ngx-tooltip

--- src/shared/components/skill-budges/skill-budges.module.ts
+++ src/shared/components/skill-budges/skill-budges.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SkillBudgesComponent } from './skill-budges.component';
+import { TooltipModule } from 'ngx-bootstrap/tooltip/';
 
 
 
 @NgModule({
   declarations: [SkillBudgesComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    TooltipModule.forRoot()
   ],
   exports: [
     SkillBudgesComponent
