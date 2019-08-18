885ccf926cb554934721ccf33e0714ec4b866380
loukas.kotas <loukas.kotas@gmail.com>
2019-08-18 20:16:34 +0300

    remove rotated header star

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -16,18 +16,7 @@ export class HeaderComponent implements OnInit {
     this.scrollToTopCounter = 0;
     this.onScroll();
     setTimeout(() => {
-      this.rotateIcon();
     }, 1000);
-  }
-
-  rotateIcon(): void {
-    if ( this.scrollToTopCounter > 0 && this.lastKnownScrollPosition === 0) {
-      const iconEl = document.getElementById('brand-icon') as HTMLElement;
-      iconEl.classList.remove('navbar-brand--animation');
-      setTimeout(() => {
-        iconEl.classList.add('navbar-brand--animation');
-      }, 20);
-    }
   }
 
   onScroll(): void {
@@ -36,7 +25,6 @@ export class HeaderComponent implements OnInit {
       if (this.lastKnownScrollPosition === 0) {
         this.scrollToTopCounter++;
       }
-      this.rotateIcon();
     });
   }
 
