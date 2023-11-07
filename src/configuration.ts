671a420e95851ce32a2edc9f917360f352e7ab29
Loukas Kotas <loukas.kotas@gmail.com>
2023-11-07 19:18:17 +0200

    change basic colors

--- src/configuration.ts
+++ src/configuration.ts
@@ -3,8 +3,8 @@ export interface About {
 }
 
 enum Side {
-  left = 'left',
-  right = 'right',
+  left = "left",
+  right = "right",
 }
 
 interface Company {
@@ -35,120 +35,158 @@ export interface Badge {
 }
 
 interface ConfigData {
-    about: About;
-    experience: Experience;
-    skills: Skills;
-    badges: Badge[];
+  about: About;
+  experience: Experience;
+  skills: Skills;
+  badges: Badge[];
+}
+
+const baseColor = "#FA5B3C";
+
+const colorShades = [
+  baseColor, // Base color
+  shadeColor(baseColor, -20), // Slightly darker shade
+  shadeColor(baseColor, -40), // Even darker shade
+  shadeColor(baseColor, 20), // Slightly lighter shade
+  shadeColor(baseColor, 40), // Even lighter shade
+];
+
+function shadeColor(color, percent) {
+  var usePound = false;
+
+  if (color[0] == "#") {
+    color = color.slice(1);
+    usePound = true;
+  }
+
+  var num = parseInt(color, 16);
+
+  var r = (num >> 16) + percent;
+
+  if (r > 255) r = 255;
+  else if (r < 0) r = 0;
+
+  var b = ((num >> 8) & 0x00ff) + percent;
+
+  if (b > 255) b = 255;
+  else if (b < 0) b = 0;
+
+  var g = (num & 0x0000ff) + percent;
+
+  if (g > 255) g = 255;
+  else if (g < 0) g = 0;
+
+  return (usePound ? "#" : "") + (g | (b << 8) | (r << 16)).toString(16);
 }
 
 export const configData: ConfigData = {
   about: {
-    description: 'Loukas is an ambitious and interested graduate EECE student with discipline and accountability involved into open-source projects, Volunteering activities and a lot of code. His motto is "Gain through Experiencing" and he applies that in his every-day life and work. His personal development comes through perseverance and avoidance of superficial knowledge. The meaning of true success comes from his team’s welfare. Using critical and organizing mind, his concise and impeccable thoughts produce succinct and cater solutions. His next goal is to overcome the novice level and become an expert in his field of interest.',
+    description:
+      'Loukas is an ambitious and interested graduate EECE student with discipline and accountability involved into open-source projects, Volunteering activities and a lot of code. His motto is "Gain through Experiencing" and he applies that in his every-day life and work. His personal development comes through perseverance and avoidance of superficial knowledge. The meaning of true success comes from his team’s welfare. Using critical and organizing mind, his concise and impeccable thoughts produce succinct and cater solutions. His next goal is to overcome the novice level and become an expert in his field of interest.',
   },
   experience: {
-    title: 'Experience',
+    title: "Experience",
     companies: [
-    {
-      duration: 'July 2022 - Now',
-      title: 'Betsson Group',
-      subtitle: 'Software Engineer',
-      side: Side.left,
-    },
-    {
-      duration: 'July 2021 - July 2022',
-      title: 'Benefit Software',
-      subtitle: 'Web Front End Developer',
-      side: Side.right,
-    },
-    {
-      duration: 'May 2018 - July 2021',
-      title: 'noris M.I.K.E',
-      subtitle: 'Web Front End Developer',
-      side: Side.left,
-    },
-    {
-      duration: 'April 2017 - May 2017',
-      title: 'More than Themes',
-      subtitle: 'Software Engineer',
-      side: Side.right,
-    },
-    {
-      duration: 'Feb 2017 - Sep 2017',
-      title: 'IEEE',
-      subtitle: 'Web Application Developer',
-      side: Side.left,
-    },
-    {
-      duration: 'Feb 2016 - Sep 2016',
-      title: 'IEEE',
-      subtitle: 'Web Application Developer',
-      side: Side.right,
-    },
-    {
-      duration: 'Oct 2012 - Oct 2020',
-      title: 'Democritus University of Thrace (DUTh)',
-      subtitle: 'BSc. & Integrated MSc. Electrical Engineering & Computer Science',
-      side: Side.left,
-    },
-  ]},
+      {
+        duration: "July 2022 - Now",
+        title: "Betsson Group",
+        subtitle: "Software Engineer",
+        side: Side.left,
+      },
+      {
+        duration: "July 2021 - July 2022",
+        title: "Benefit Software",
+        subtitle: "Web Front End Developer",
+        side: Side.right,
+      },
+      {
+        duration: "May 2018 - July 2021",
+        title: "noris M.I.K.E",
+        subtitle: "Web Front End Developer",
+        side: Side.left,
+      },
+      {
+        duration: "April 2017 - May 2017",
+        title: "More than Themes",
+        subtitle: "Software Engineer",
+        side: Side.right,
+      },
+      {
+        duration: "Feb 2017 - Sep 2017",
+        title: "IEEE",
+        subtitle: "Web Application Developer",
+        side: Side.left,
+      },
+      {
+        duration: "Feb 2016 - Sep 2016",
+        title: "IEEE",
+        subtitle: "Web Application Developer",
+        side: Side.right,
+      },
+      {
+        duration: "Oct 2012 - Oct 2020",
+        title: "Democritus University of Thrace (DUTh)",
+        subtitle:
+          "BSc. & Integrated MSc. Electrical Engineering & Computer Science",
+        side: Side.left,
+      },
+    ],
+  },
   skills: {
-    title: 'Skills',
+    title: "Skills",
     techs: [
       {
-        id: 'angular',
+        id: "angular",
         value: 95,
-        title: 'Angular',
-        circleColor: '#03045e',
+        title: "Angular",
+        circleColor: colorShades[0],
       },
       {
-        id: 'javascript',
+        id: "javascript",
         value: 90,
-        title: 'Javascript',
-        circleColor: '#023e8a',
+        title: "Javascript",
+        circleColor: colorShades[1], // Slightly darker shade
       },
       {
-        id: 'css',
+        id: "css",
         value: 80,
-        title: 'CSS',
-        circleColor: '#0077b6',
+        title: "CSS",
+        circleColor: colorShades[2], // Even darker shade
       },
       {
-        id: 'nodejs',
+        id: "nodejs",
         value: 80,
-        title: 'Node.js',
-        circleColor: '#0096c7',
+        title: "Node.js",
+        circleColor: colorShades[3], // Slightly lighter shade
       },
       {
-        id: 'photoshop',
+        id: "photoshop",
         value: 70,
-        title: 'Photoshop',
-        circleColor: '#00b4d8',
-      }
+        title: "Photoshop",
+        circleColor: colorShades[4], // Even lighter shade
+      },
     ],
   },
   badges: [
     {
-      title: 'Testing Tools',
-      iconTitles: ['jasmine', 'protractor'],
-      tooltipTitles: ['Jasmine', 'Protractor'],
+      title: "Testing Tools",
+      iconTitles: ["jasmine", "protractor"],
+      tooltipTitles: ["Jasmine", "Protractor"],
     },
     {
-      title: 'Other Tools',
-      iconTitles: ['bootstrap', 'd3js', 'webpack', 'travis', 'git'],
-      tooltipTitles: ['Bootstrap', 'D3js', 'Webpack', 'Travis', 'Git'],
+      title: "Other Tools",
+      iconTitles: ["bootstrap", "d3js", "webpack", "travis", "git"],
+      tooltipTitles: ["Bootstrap", "D3js", "Webpack", "Travis", "Git"],
     },
     {
-      title: 'Databases',
-      iconTitles: ['postgresql', 'mongodb', 'mysql'],
-      tooltipTitles: ['PostgreSQL', 'MongoDB', 'MySQL'],
+      title: "Databases",
+      iconTitles: ["postgresql", "mongodb", "mysql"],
+      tooltipTitles: ["PostgreSQL", "MongoDB", "MySQL"],
     },
     {
-      title: 'OS',
-      iconTitles: ['ubuntu', 'apple'],
-      tooltipTitles: ['Ubuntu', 'MacOS'],
+      title: "OS",
+      iconTitles: ["ubuntu", "apple"],
+      tooltipTitles: ["Ubuntu", "MacOS"],
     },
-  ]
-
+  ],
 };
-
-
